08e089edba3b6f54ba9da3eac6070fbd58d8300f
Neel Mehta <neelmehta0086@gmail.com>
2022-09-25 18:05:51 +0530

    2nd commit

--- src/TreeTools.js/Tree.js
+++ src/TreeTools.js/Tree.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Modal, Form } from 'react-bootstrap';
 import './style.css'
  function Tree() {
   const [inpArray,setInpArray]=useState(String)
